0838ee055bcde13e6999568a09f055d181fa212f
nijuma <nijuma@protonmail.com>
2022-08-23 15:41:50 -0400

    who even looks at this

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ playing = true;
 let paper = document.querySelector('.paper');
 let scissors = document.querySelector('.scissors');
 let rock = document.querySelector('.rock');
-
-var img = document.createElement('img');
-img.src = 'imgs/noun-scissors-477919.png';
+let score1 = document.querySelector('.score--1');
+let score2 = document.querySelector('.score--2');
+let tie = document.querySelector('.tie');
+let overlay = document.querySelector('.overlay');
 
 var choices = ['rock', 'paper', 'scissors'];
 let userChoice = '';
+
+let userScore = 0;
+let cpuScore = 0;
+const maxScore = 5;
 
 const cpuRoll = Math.trunc(Math.random() * 3) + 1;
 
@@ -26,6 +31,18 @@ const show = () => {
   rock.classList.toggle('hidden');
   paper.classList.toggle('hidden');
   scissors.classList.toggle('hidden');
+  score1.classList.add('hidden');
+  score2.classList.add('hidden');
+  tie.classList.add('hidden');
+};
+
+const hide = () => {
+  score1.classList.remove('hidden');
+  score2.classList.remove('hidden');
+};
+const hideAgain = () => {
+  score1.classList.add('hidden');
+  score2.classList.add('hidden');
 };
 
 show();
@@ -38,22 +55,38 @@ document.querySelector('#play--game').addEventListener('click', function () {
 
 function logic(userChoice) {
   const cpuRoll = Math.trunc(Math.random() * 3) + 1;
-
+  hide();
   console.log(userChoice);
   console.log(cpuRPS(cpuRoll));
 
   if (userChoice === cpuRPS(cpuRoll)) {
     console.log('tie');
+    hide();
+    tie.classList.remove('hidden');
+    hideAgain();
   } else if (userChoice === 'rock' && cpuRPS(cpuRoll) === 'scissors') {
+    tie.classList.add('hidden');
     console.log('win');
+    userScore++;
+    document.querySelector('.score--2').textContent = userScore;
   } else if (userChoice === 'scissors' && cpuRPS(cpuRoll) === 'paper') {
+    tie.classList.add('hidden');
     console.log('win');
+    userScore++;
+    document.querySelector('.score--2').textContent = userScore;
   } else if (userChoice === 'paper' && cpuRPS(cpuRoll) === 'rock') {
+    tie.classList.add('hidden');
     console.log('win');
+    userScore++;
+    document.querySelector('.score--2').textContent = userScore;
   } else {
+    tie.classList.add('hidden');
     console.log('lose');
+    cpuScore++;
+    document.querySelector('.score--1').textContent = cpuScore;
   }
 }
+
 document.querySelector('.rock').onclick = () => {
   userChoice = 'rock';
   //after user choice, we randomize PC choice
@@ -72,10 +105,22 @@ document.querySelector('.paper').onclick = () => {
 
 document.querySelector('.scissors').onclick = () => {
   userChoice = 'scissors';
-  document.querySelector('.scissors').innerHTML = img;
-
   //after user choice, we randomize PC choice
   cpuRPS(cpuRoll);
   //check who wins
   logic(userChoice, cpuRPS(cpuRoll));
 };
+
+const overlayModal = () => {
+  overlay.classList.add('active');
+};
+const gameOver = () => {
+  return userScore === 5 || cpuScore === 5;
+};
+
+const restart = () => {
+  playing = false;
+  userScore = 0;
+  cpuScore = 0;
+};
+gameOver(overlayModal);
